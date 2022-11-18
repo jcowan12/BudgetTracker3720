c9a167e8483d4602260acf24506501a5c0386fe8
jonat <jcowan@clemson.edu>
2022-11-18 11:55:45 -0500

    started implementing api

--- src/App.js
+++ src/App.js
@@ -5,11 +5,4 @@ import { useEffect } from 'react';
 
 const API_URL = 'https://dev.lunchmoney.app/v1/categories?access_token=af33c6052dbb2d6e2d8544e007ff13c665c540b3b42f6a6bc4';
 
-const App = () => {
 
-    return (
-        <h1>App</h1>
-    );
-}
-
-export default App;
