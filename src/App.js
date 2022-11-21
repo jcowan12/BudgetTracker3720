dee436acf4cd1d6bb5519ecb0d235bae4c9db9b8
jonat <jcowan@clemson.edu>
2022-11-21 10:52:36 -0500

    preparing for UI build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { useEffect } from 'react';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    return <p>Hello React!</p>;
+};
+
+export default App;
 
 
 
