9c1817d49529df4d0a0f2f4a28adc24453480ae6
jonat <jcowan@clemson.edu>
2022-11-21 10:21:13 -0500

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { useEffect } from 'react';
 
-// lunch money api key
-// af33c6052dbb2d6e2d8544e007ff13c665c540b3b42f6a6bc4
 
-const API_URL = 'https://dev.lunchmoney.app/v1/categories?access_token=af33c6052dbb2d6e2d8544e007ff13c665c540b3b42f6a6bc4';
 
 
