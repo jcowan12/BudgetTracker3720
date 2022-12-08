a5267987bfbb426bd7ece8d96e314ce0541b8f0d
jonat <jcowan@clemson.edu>
2022-12-07 19:58:02 -0500

    troubleshooting

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
     <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={"https://jcowan12.github.io/"}>
+    redirectUri={'https://jcowan12.github.io'}>
         <App />
     </Auth0Provider>,
     document.getElementById('root')
